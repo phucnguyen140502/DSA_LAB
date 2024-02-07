058bd22b69856c3dde9146b62c7e43be4338f299
phucnguyen140502 <nguyenducnguyenphuc@gmail.com>
2024-02-07 18:40:09 +0700

    update

---
+++ 21108_NDNguyenPhuc_Lab6/src/Hashtable/Hashtable.java
@@ -0,0 +1,14 @@
+package Hashtable;
+
+public class Hashtable<K, V> {
+
+    private K key;
+    private V value;
+
+    public Hashtable(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    
+}
